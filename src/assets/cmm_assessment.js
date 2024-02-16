81819a103b7a1e141224eef9e5eb18a5a70c46da
Chris Nesbitt-Smith <chris@cns.me.uk>
2024-02-16 09:52:06 +0000

    stub tests

--- src/assets/cmm_assessment.js
+++ src/assets/cmm_assessment.js
@@ -11,7 +11,6 @@ document
   );
 
 function clearFormValues() {
-  console.log("fo");
   localStorage.clear();
 }
 
---
+++ tests/cmm_assessment.test.js
@@ -0,0 +1,18 @@
+const m = require("../src/assets/cmm_assessment.js");
+
+describe("clearFormValues", () => {
+  it.todo("should clear the stored state");
+  it.todo("should ask for confirmation before acting");
+});
+
+describe("restoreFormValues", () => {
+  it.todo("should restore form values to expected state");
+});
+
+describe("eventListeners", () => {
+  it.todo("should add event listeners to the form");
+});
+
+describe("Store things", () => {
+  it.todo("store things");
+});
